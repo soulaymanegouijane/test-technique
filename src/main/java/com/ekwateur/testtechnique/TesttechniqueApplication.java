5059cfe6dd64774d5342805d0b1acf2831932467
soulaymanegouijane <soulaymanegouijane@gmail.com>
2023-10-04 22:49:32 +0200

    initial commit

---
+++ src/main/java/com/ekwateur/testtechnique/TesttechniqueApplication.java
@@ -0,0 +1,13 @@
+package com.ekwateur.testtechnique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesttechniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesttechniqueApplication.class, args);
+	}
+
+}
